172d519597b436f57dfd22e66fd069ca0cbfe60c
dohyunkim <rlaehgus10000@daum.net>
2023-01-04 20:42:31 +0900

    #23 - 게시판 페이지에 decoupled logic 적용
    타임리프 문법을 HTML 마크업 페이지에서 분리해서 th.xml파이로 옮김

--- src/main/java/com/bitstudy/app/AppApplication.java
+++ src/main/java/com/bitstudy/app/AppApplication.java
@@ -2,8 +2,11 @@ package com.bitstudy.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan /* config > ThymeleafConfig 파일에서 필요 */
 public class AppApplication {
 
 	public static void main(String[] args) {
