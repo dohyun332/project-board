4bed7ebb7df88ff789f8d37c10b3dac27d13f15a
dohyunkim <rlaehgus10000@daum.net>
2023-01-10 19:32:29 +0900

    #31 - 게시판 컨트롤러 구현
    게시판 리스트 화면 조회를 위한 컨트롤러를 구현.
    모델을 이용한 데이터 전달 완료

--- src/main/java/com/bitstudy/app/controller/ArticleController.java
+++ src/main/java/com/bitstudy/app/controller/ArticleController.java
@@ -1,6 +1,14 @@
 package com.bitstudy.app.controller;
 
 
+import com.bitstudy.app.domain.type.SearchType;
+import com.bitstudy.app.dto.response.ArticleResponse;
+import com.bitstudy.app.dto.response.ArticleWithCommentsResponse;
+import com.bitstudy.app.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
@@ -25,25 +33,57 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * 그외에 css, img 그리고 js들은 resources > static 폴더 안에 작성가능
  * */
 
+@RequiredArgsConstructor // 필수 필드에 대한 생성자 자동생성
+//@RequiredArgsConstructor 초기화 되지않은 final 필드 또는 @Nonnull이 붙은 필드에 대해 생성자 생성 lombok애너테이션
 @Controller
 @RequestMapping("/articles") // 모든 경로들은 /articles로 시작하니까 클래스 레벨에 1차로 @RequestMapping("/articles") 걸어놨음
 public class ArticleController {
+
+    private final ArticleService articleService;
+    /* @RequiredArgsConstructor로 만들어진 생성자(여기서는 articleService)를 얘가
+    * 읽어서 정보의 전달을 할 수 있게한다. */
+
     /* BDD하러 가기*/
+    /** 할일 나타낼때 주석임 마우스 올리면 해당 설명 나온다. */
+    /* 게시판 리스트 보여주기 */
     @GetMapping
-    public String articles(ModelMap map) {
+    public String articles(
+            @RequestParam(required = false) SearchType searchType,
+            @RequestParam(required = false) String searchValue,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            /** 해설:
+             *    @RequestParam: 검색어를 받아야한다. @RequestParam을 이용해서        getParameter를 불러올거고, 반드시 있지 않아도 된다.
+             *    없으면 게시글 전체 조회하면 되니까 SearchType, searchValue에
+             *   (required = false) 달아서 null 들어올수 있게 걸어놓을수 있다.
+             *    @PageableDefault: 페이징 기본설정(한페이지에 10개, 작성일순, 내     림차순)
+             * */
+            ModelMap map) {
+
+
         /** ModelMap: 테스트파일에서 .andExpect(model().attributeExists("articles"));를 이용해서 articles라는 키 값으로
          *  데이터를 넣어주기로 했으니까 필요함
          *  Model과 ModelMap은 같은거임. 차이점은 Model은 인터페이스, ModelMap은 클래스(구현체)
          *  */
         map.addAttribute("articles", List.of()); // 키: articles, 값: 그냥 list
+
+        map.addAttribute("articles", articleService.searchArticles(searchType,searchValue,pageable).map(ArticleResponse::from));
+        // 진짜로 정보를 넣어줘야하니까 ArticleService.java에 만들어 놓은 searchArticles()메서드에 값을 넣어주면 됨
+        // 그런데 searchArticles()의 반환 타입은 DTO인데 dto는 모든 엔티티의데이터를 다 다루고 있어서 그걸 한번 더 가공해서 필요한 것들만 쓸거다. 그래서 게시글 내용만 가지고 있는 ArticleResponse를 사용한다.
         return "articles/index";
     }
 
+    /* 게시글 상세 페이지*/
     @GetMapping("/{articleId}")
     public String article(ModelMap map, @PathVariable Long articleId) {
-        map.addAttribute("article", "aaa");
+        ArticleWithCommentsResponse article =
+                ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
+
+        map.addAttribute("article", "article");
+//        map.addAttribute("article", "aaa");
         // 지금당장은 받아오지 않기 때문에 null이라고 넣었지만, 테스트할때는 뭐라도 문자열을 넣어서 모델에 담기도록한다.
-        map.addAttribute("articleComments", List.of());
+        map.addAttribute("articleComments", article.articleCommentsResponse());
+        // article.articleCommentsResponse() 해설: 현재 article에  article.articleCommentsResponse의 정보가 담겨있으니까 article안에 있는 articleComments를 꺼내면 된다.
+//        map.addAttribute("articleComments", List.of());
 
         return "articles/detail";
     }
