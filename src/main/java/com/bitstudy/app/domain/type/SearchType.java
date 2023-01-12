06f907328af64faba4e497d7d2951a6f89091152
dohyunkim <rlaehgus10000@daum.net>
2023-01-12 18:13:22 +0900

    #38 - 게시판 페이지 검색바 기능과 테스트 업데이트
    게시판 페이지의 드롭다운 메뉴를 서버에서 받아서 쓰도록 th.xml변경
    SearchType.java(enum)을 한글로 출력되도록 수정

--- src/main/java/com/bitstudy/app/domain/type/SearchType.java
+++ src/main/java/com/bitstudy/app/domain/type/SearchType.java
@@ -1,6 +1,23 @@
 package com.bitstudy.app.domain.type;
 
+import lombok.Getter;
+
 public enum SearchType {
 //    제목, 본문, id, 글쓴이 ,해시태그
-    TITLE, CONTENT, ID, NICKNAME, HASHTAG
+//    TITLE, CONTENT, ID, NICKNAME, HASHTAG
+    TITLE("제목2"),
+    CONTENT("본문2"),
+    ID("유저아이디2"),
+    NICKNAME("닉네임2"),
+    HASHTAG("해시태그2");
+
+    // name(description)
+
+    @Getter
+    private final String description;
+
+
+    SearchType(String description) {
+        this.description = description;
+    }
 }
